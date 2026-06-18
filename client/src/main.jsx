497b14e33cc3d29abeab8ac6eba31d28f37fe05a
Mr-Noredine <mohammedinoredine@gmail.com>
2026-06-18 12:41:15 +0200

    Prepare deployment configuration

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { GoogleOAuthProvider } from '@react-oauth/google'
+import axios from 'axios'
 import App from './App.jsx'
 import { AuthProvider } from './contexts/AuthContext.jsx'
 import './styles/global.css'
+
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL || ''
 
 const app = (
   <BrowserRouter>
@@ -12,13 +15,13 @@ const app = (
       <App />
     </AuthProvider>
   </BrowserRouter>
-);
+)
 
 const root = import.meta.env.VITE_GOOGLE_CLIENT_ID ? (
   <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
     {app}
   </GoogleOAuthProvider>
-) : app;
+) : app
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>{root}</React.StrictMode>,
